0aba8b1c94c536bd134cdf341e6de85bf7cd608f
Maninderpal Singh <maninderpals825@gmail.com>
2020-01-23 08:16:02 +0530

    Session7 - Sequences in Python - II

---
+++ session7d.py
@@ -0,0 +1,15 @@
+"""
+    In Dicitionary we have indexes replaced with Keys
+    Keys can be of our choice
+    and they must be always string
+
+"""
+
+student = {
+    "rollnum": 101,
+    "name": "john",
+    "email": "john@example.com"
+}
+
+print(student)
+print(type(student))
